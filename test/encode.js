74ce1563d01ff9f282e8c6ea12f2c483d173617a
William Casarin <bill@casarin.me>
2015-02-16 16:17:28 -0800

    add initial tests, fixtures, etc

--- test/encode.js
+++ test/encode.js
@@ -1,16 +1,37 @@
 var test = require('tape')
+var join = require('path').join
 var encode = require('../lib/encode')
+var clone = require('clone')
+var count = require('stream-count')
+var hashStream = require('hash-stream')
+
+var settings = {
+  format: 'mp3',
+  src: join(__dirname, 'fixtures/hellberg.mp3'),
+  metadata: {
+    title: 'こんにちは世界',
+    author: 'Hellberg',
+    trackNumber: 10,
+    album: 'Hellberg EP',
+    genre: 'Electronic',
+    comment: 'This is my jam',
+    artwork: join(__dirname, 'fixtures/art.png')
+  }
+}
 
 function testFormat (fmt) {
   test('conversion to ' + fmt + ' works', function (t) {
-    t.plan(1)
+    t.plan(2)
 
-    var encoder = encode({
-      
+    var opts = clone(settings)
+    opts.format = fmt
+    var stream = encode(opts)
+
+    hashStream(stream, 'md5', function (err, hash) {
+      t.ok(err)
+      t.equal(hash, "hey")
     })
   })
 }
 
-test('encoding', function (t) {
-  
-})
+testFormat('mp3')
