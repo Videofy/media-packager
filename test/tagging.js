47315bc34868ab18dea50f1c1c0659f880c3b2de
William Casarin <bill@casarin.me>
2015-02-18 17:29:58 -0800

    file copy for utils that don't support streams

--- test/tagging.js
+++ test/tagging.js
@@ -3,12 +3,10 @@ var test = require('tape')
 var join = require('path').join
 var fs = require('fs')
 var tagger = require('../lib/tagger')
-var clone = require('clone')
 var mm = require('musicmetadata')
-var debug = require('debug')('media-packager:test:encode')
+var debug = require('debug')('media-packager:test:tagging')
 var rimraf = require('rimraf').sync
 
-var dst = join(__dirname, 'fixtures/var/hellberg_out.mp3')
 var metadata = {
   artwork: join(__dirname, 'fixtures/art.png'),
   title: 'what',
@@ -22,6 +20,8 @@ var metadata = {
 function testTagging (fmt) {
   test(fmt + ' tagging works', function (t) {
     t.plan(6)
+
+    debug('got here')
 
     var stream = fs.createReadStream(src)
                  .pipe(tagger(metadata))
