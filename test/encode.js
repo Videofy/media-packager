9a584bfb23bdd2c5c85d165ecc1de0f34ff49dd6
William Casarin <bill@casarin.me>
2015-02-27 11:36:48 -0800

    fix

--- test/encode.js
+++ test/encode.js
@@ -18,7 +18,7 @@ function rouglyEqual(t, actual, expected, bound, userMsg) {
   var error = actual - expected
   var diff = Math.abs(error)
   userMsg = userMsg ? userMsg + ": " : ""
-  var msg = util.format('%sactual %d ~= %d (±%d) within ±%d bound', msg, actual, expected, diff, bound)
+  var msg = util.format('%sactual %d ~= %d (±%d) within ±%d bound', userMsg, actual, expected, diff, bound)
   t.ok(diff <= bound, msg)
 }
 
